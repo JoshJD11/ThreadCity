300ba42c9a30e8c73a0a9a5814f79a4d096e7568
Sebas Quesada <167464285+Sebco27@users.noreply.github.com>
2025-11-03 01:31:42 -0600

    add Vehicle structure
    Implement the Vehicle structure with its own type, current street and route to follow

---
+++ project/src/vehicle.rs
@@ -0,0 +1,39 @@
+use std::collections::VecDeque;
+use crate::city::StreetId;
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum VehicleType {
+    Car,
+    Ambulance,
+    Truck
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct Vehicle {
+    pub vehicle_type: VehicleType,
+    pub current_street: StreetId,
+    pub route: VecDeque<StreetId>
+}
+
+impl Vehicle {
+    pub fn new(vehicle_type: VehicleType, mut route: VecDeque<StreetId>) -> Self {
+        Self {
+            vehicle_type,
+            current_street: route.pop_front().unwrap(),
+            route
+        }
+    }
+
+    pub fn set_street(&mut self, street: StreetId) {
+        self.current_street = street;
+    }
+
+    pub fn next_street(&self) -> Option<&StreetId> {
+        self.route.front()
+    }
+
+    pub fn move_forward(&mut self){
+        let next_street = self.route.pop_front().unwrap();
+        self.set_street(next_street);
+    }
+}
