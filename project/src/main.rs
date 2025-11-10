1e08de4ba55fb9d99aa27c1872745f4b7e08fc8c
Sebco27 <167464285+Sebco27@users.noreply.github.com>
2025-11-10 16:37:24 -0600

    add basic main.rs for city simulation testing

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,36 +1,17 @@
-use std::ffi::c_void;
+mod grid_generator;
+mod architect;
+mod city;
+mod mymutex;
+mod vehicle;
+mod simulation;
 
-enum State {
-    Start,
-    Running,
-    Stopped,
-}
+use gtk::prelude::{ApplicationExt, ApplicationExtManual};
+use gtk::{Application};
 
-struct thread {
-    id: i32,
-    state: State,
-    func_ptr: Option<extern "C" fn(arg: *mut c_void) -> *mut c_void> // hay que usar funciones con extern "C" para poder usarse.
-}
+const APP_ID: &str = "org.gtk.city";
 
-
-extern "C" fn devuelve_valores(_arg: *mut c_void) -> *mut c_void {
-    let mut datos = vec![10, 20, 30]; 
-    let ptr = datos.as_mut_ptr();
-    std::mem::forget(datos); 
-    ptr as *mut c_void
-}
-
-fn main() {
-    let func: Option<extern "C" fn(*mut c_void) -> *mut c_void> = Some(devuelve_valores);
-
-    if let Some(f) = func {
-        let result_ptr = f(std::ptr::null_mut()); 
-
-        unsafe {
-            let valores: *mut i32 = result_ptr as *mut i32;
-            for i in 0..3 {
-                println!("Valor {}: {}", i, *valores.add(i));
-            }
-        }
-    }
+fn main() -> glib::ExitCode {
+    let simulation = Application::builder().application_id(APP_ID).build();
+    simulation.connect_activate(grid_generator::generate_grid);
+    simulation.run()
 }
